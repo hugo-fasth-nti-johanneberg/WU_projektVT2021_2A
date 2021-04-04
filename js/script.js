01ee72ec7b4f916a9eb7cc8b592812ab0a9bf4f4
hugo-fasth-nti-johanneberg <hugo.fasth@elev.ga.ntig.se>
2021-04-04 23:37:30 +0200

    Dessa två dagarna har jag fixat hamburgarmenyn och resterande css samt fixat alla html sidor

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-const burger = document.querySelector('.burger')
-const nav = document.querySelector('.navigational-links')
-
-function toggleMeny() {
-    nav.classList.toggle('nav-active')
-    burger.classList.toggle('toggle')
+// const menuBtn = document.querySelector('.menu-btn');
+// let menuOpen = false;
+// menuBtn.addEventListener('click', () => {
+//     if(!menuOpen) {
+//         menuBtn.classList.add('open');
+//         menuOpen = true;
+//     } else {
+//         menuBtn.classList.remove('open');
+//         menuOpen = false;
+//     }
+// });
+function toggleClass(className) {
+    const body = document.querySelector("body");
+    
+    if (!body.classList.contains(className)) {
+        body.classList.add(className);
+    }
+    else {
+        body.classList.remove(className);
+    }
 }
 
-burger.addEventListener('click', toggleMeny)
+function toggle(){
+    toggleClass('openButton');
+    console.log("toggle called");
+}
+
+const buttonElement = document.getElementsByClassName("menu-btn")[0];
+buttonElement.addEventListener("click", toggle);
